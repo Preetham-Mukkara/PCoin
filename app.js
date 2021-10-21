8baf63c24dc09a40996a8ed6c8981d342d707bec
Preetham <mukkarapreetham@gmail.com>
2021-10-21 16:14:59 -0500

    commiting changes

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 8000;
   
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
   
-const PORT = process.env.PORT || 8000;
+app.post("/test",(req,res)=>{
+    res.send("posting arbitrary data!");
+});
   
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
